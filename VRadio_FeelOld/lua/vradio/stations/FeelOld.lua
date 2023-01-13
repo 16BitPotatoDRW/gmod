1bbdee0fb29aab7781e3640d40bc49aac4700783
16BitPotatoDRW <119771134+16BitPotatoDRW@users.noreply.github.com>
2023-01-13 16:58:49 -0600

    Add files via upload

---
+++ VRadio_FeelOld/lua/vradio/stations/FeelOld.lua
@@ -0,0 +1,26 @@
+local STANTIONS = {
+	[0] = {
+		['meta'] = {
+			['name'] = "FeelOld 201.1",
+			['icon'] = "gui/vehicles_radio_icons/FeelOld.png"
+		},
+		['tracks'] = {
+			[0] = "https://ia600606.us.archive.org/24/items/jamendo-086527/01.mp3",
+			[1] = "https://ia802309.us.archive.org/8/items/vengaboys-we-like-to-party/We%20Like%20To%20Party%20%28The%20Vengabus%29/01-We%20Like%20To%20Party%21%20%28Airplay%29.mp3",
+			[2] = "https://ia600907.us.archive.org/2/items/tvtunes_26709/Crazy%20Frog%20-%20Axel%20F.mp3",
+			[3] = "https://ia601804.us.archive.org/33/items/the-hampster-dance-song/The%20HampsterDance%20Song.mp3",
+			[4] = "https://ia802705.us.archive.org/22/items/iuma-darude/Darude_-_Sandstorm.mp3",
+			[5] = "https://ia804502.us.archive.org/31/items/alice-deejay-better-off-alone/Alice%20Deejay/Better%20Off%20%20Alone/01-Better%20Off%20Alone%20%28Radio%20Edit%29.mp3",
+			[6] = "https://ia800307.us.archive.org/27/items/Dragostea_Din_Tei_1/OZoneDragosteadintei.mp3",
+			[7] = "https://ia902708.us.archive.org/0/items/WeNoSpeakAmericano_301/01WeNoSpeakAmericano.mp3",
+			[8] = "https://ia800202.us.archive.org/1/items/EurodancerRemix/EurodancerRemix.mp3",
+			[9] = "https://ia803001.us.archive.org/8/items/LoitumaIevanPolkka/Loituma-%20Ievan%20Polkka.mp3",
+			[10] = "https://ia601408.us.archive.org/25/items/blue-da-ba-dee-video-edit/Blue%20%28Da%20Ba%20Dee%29%20%28Video%20Edit%29.mp3",
+			[11] = "https://ia904504.us.archive.org/9/items/snap-rhythm-is-a-dancer-1992/Rhythm%20Is%20A%20Dancer/02-Rhythm%20Is%20A%20Dancer%20%2812%27%27%20Mix%29.mp3",
+			[12] = "https://ia600906.us.archive.org/28/items/tvtunes_19937/Spongebob%20Squarepants%20-%20APM%20-%20Mark%20Governor%20-%20Stadium%20Rave%20A.mp3",
+			[13] = "https://ia804507.us.archive.org/0/items/vol.-01-30-2013-2017/%D0%9D%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%96%D0%B0%D0%B6%D0%B4%D0%B0%20%D0%A1%D0%BA%D0%BE%D1%80%D0%BE%D1%81%D1%82%D0%B8%20Vol.%2001%20-%2030%20%282013-2017%29/Novaya_Zazhda_Skorosti_vol_9_-_2014/04.%20DCX%20-%20Flying%20High%20%28RaymanRave%20%26%20BloodDropz%20Remix%29.mp3"
+		}
+	}
+}
+
+VRADIO:IncludeStation(STANTIONS)
---
+++ VRadio_MGR/lua/vradio/stations/MGR.lua
@@ -0,0 +1,24 @@
+local STANTIONS = {
+	[0] = {
+		['meta'] = {
+			['name'] = "DNA FM",
+			['icon'] = "gui/vehicles_radio_icons/MGR.png"
+		},
+		['tracks'] = {
+			[0] = "https://ia804505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/01.%20Rules%20of%20Nature%20%28Platinum%20Mix%29.mp3",
+			[1] = "https://ia804505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/02.%20The%20Only%20Thing%20I%20Know%20For%20Real%20%28Maniac%20Agenda%20Mix%29.mp3",
+			[2] = "https://ia804505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/03.%20Dark%20Skies%20%28Platinum%20Mix%29.mp3",
+			[3] = "https://ia804505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/04.%20I%27m%20My%20Own%20Master%20Now%20%28Platinum%20Mix%29.mp3",
+			[4] = "https://ia904505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/05.%20A%20Stranger%20I%20Remain%20%28Maniac%20Agenda%20Mix%29.mp3",
+			[5] = "https://ia904505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/06.%20Return%20to%20Ashes%20%28Platinum%20Mix%29.mp3",
+			[6] = "https://ia804505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/07.%20The%20Stains%20of%20Time%20%28Maniac%20Agenda%20Mix%29.mp3",
+			[7] = "https://ia904505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/08.%20Red%20Sun%20%28Maniac%20Agenda%20Mix%29.mp3",
+			[8] = "https://ia904505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/09.%20A%20Soul%20Can%27t%20Be%20Cut%20%28Platinum%20Mix%29.mp3",
+			[9] = "https://ia804505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/10.%20Collective%20Consciousness%20%28Maniac%20Agenda%20Mix%29.mp3",
+			[10] = "https://ia904505.us.archive.org/20/items/metal-gear-rising-revengeance-vocal-tracks/11.%20It%20Has%20To%20Be%20This%20Way%20%28Platinum%20Mix%29.mp3",
+			[11] = "https://ia802305.us.archive.org/8/items/01.-metal-gear-20-years-history-past-present-future/Metal%20Gear%20Soundtrack%20Collection%20%5BFLAC%5D/2007.07.18%20-%20Metal%20Gear%2020th%20Anniversary%20Music%20Collection/02.%20Snake%20Eater.mp3"
+		}
+	}
+}
+
+VRADIO:IncludeStation(STANTIONS)
